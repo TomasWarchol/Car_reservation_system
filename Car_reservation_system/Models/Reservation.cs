9439b0d0c782b98c7af4783152d0c82d7b38ece3
TomasWarchol <warchol.tomasz.m@gmail.com>
2025-01-09 15:26:18 +0100

    1 pack of changes

---
+++ Car_reservation_system/Models/Reservation.cs
@@ -0,0 +1,12 @@
+﻿namespace Car_reservation_system.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
